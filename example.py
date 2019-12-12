7085a8d7f29f5eda92710093b6350a34efe658e9
(masahiro) <(5bjk1119@mail.u-tokai.ac.jp)>
2019-12-12 13:21:19 +0900

    Create example.py

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
-from jointgetter.io import KinectJson2CSV
-
+from jointgetter.io import KinectJson2CSV,Csv2graph
 
 name = 'sample'
 jtc = KinectJson2CSV()
 jtc(name + '.json')
+
+ctg = Csv2graph()
+ctg.load_csv(name+'.csv')
+ctg.make_graph([3,6,9])
+ctg.to_image()
+
