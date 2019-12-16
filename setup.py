9a4a59ccfa9e13f65648da5155722e41066d5f22
Masahiro-Terada <49894677+Masahiro-Terada@users.noreply.github.com>
2019-12-17 03:07:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='jointgetter',
     version='0.0.1',
-    install_requires=['numpy', 'pandas'],
+    install_requires=['pandas', 'matplotlib'],
     packages=find_packages(),
     url='',
-    license='',
-    author='',
+    license='MIT',
+    author='Masahiro Terada',
     author_email='',
     description=''
 )
