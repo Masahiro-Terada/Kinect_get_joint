61d426034a45203cecaadaaf95dd8c5c34787d03
Masahiro-Terada <49894677+Masahiro-Terada@users.noreply.github.com>
2019-12-17 03:09:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     install_requires=['pandas', 'matplotlib'],
     packages=find_packages(),
-    url='',
+    url='https://github.com/Masahiro-Terada/Kinect_get_joint',
     license='MIT',
     author='Masahiro Terada',
     author_email='',
